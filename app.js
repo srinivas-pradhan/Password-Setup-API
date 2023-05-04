3e6948258e1964292472c2f5e80b7c2a616a3407
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-04 18:49:46 -0400

    Add morgan for stdout logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require("morgan");
 const app = express()
 const MongoDB = require('./db/mongo')
 const {PORT, MONGO_URI } = require('./config')
@@ -8,6 +9,8 @@ const secretsRouter = require('./routes/secrets')
 const AuthRouter = require('./routes/auth')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
 
 // routes
 app.use('/api/v1/accounts', accountRouter)
@@ -18,7 +21,7 @@ const start = async () => {
     try {
         await MongoDB(MONGO_URI)
         app.listen(PORT, () => 
-                console.log(`Server is listening on ${PORT} `)
+                console.log(`Server is listening on ${PORT}...`)
             )   
     } catch (error) {
         console.log(error)

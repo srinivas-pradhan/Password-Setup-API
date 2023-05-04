c01ad3d26653e2ab04e0aa8e77fdfe546598a109
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-04 10:32:54 -0400

    Initial Commit - express app up and running

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.json())
+
+const port = 5000
+
+app.listen(port, () => 
+        console.log(`Server is listening on ${port} `)
+    )   

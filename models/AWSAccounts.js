c6b300e94e962e1c38859445840d96b9038f8cc8
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-04 14:07:36 -0400

    Add Db models

---
+++ models/AWSAccounts.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const AWSAccountSchema = new mongoose.Schema(
+    {
+        AccountNumber: {
+            type: Number,
+            required: [true, 'AWS Account Number must be provided.'],
+        },
+        AccountType: {
+            type: String,
+            enum: ['default', 'prod', 'stg'],
+            default: 'default',
+        },
+        DefaultRegion: {
+            type: String,
+            enum: ['us-east-1', 'us-east-2', 'us-west-1'],
+            default: 'us-east-1',
+        }
+})
+
+module.exports = mongoose.model('AccountStore', AWSAccountSchema)

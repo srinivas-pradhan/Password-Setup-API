fa215b4cbb8f9ecee42bea83c2cd1f2203a01024
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-04 18:51:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const morgan = require("morgan");
 const app = express()
 const MongoDB = require('./db/mongo')
 const {PORT, MONGO_URI } = require('./config')
-require('dotenv').config()
 const accountRouter = require('./routes/accounts')
 const secretsRouter = require('./routes/secrets')
 const AuthRouter = require('./routes/auth')

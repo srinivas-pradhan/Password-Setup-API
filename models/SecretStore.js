09d28702e541e190799103e861de09b410a87c27
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-23 13:42:15 -0400

    Update SecretStore.js

--- models/SecretStore.js
+++ models/SecretStore.js
@@ -31,6 +31,9 @@ const SecretSchema = new mongoose.Schema(
         match: [
           /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         ]        
+      },
+      Cognito_group: {
+        type: String
       }
     },
     { timestamps: true }

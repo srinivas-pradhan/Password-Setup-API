17b44591326f0a4689209cd7660d40c350ee3e5b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-05-24 19:14:48 -0400

    Update secrets.js

--- controllers/secrets.js
+++ controllers/secrets.js
@@ -7,21 +7,26 @@ const GetKeyDetails = require('../utils/kms_get_mr_key')
 
 const CreateSecret  = async ( req, res ) => {
     if (res.locals.authenticated && (res.locals.authorized || res.locals.auth_user)) {
-        const Acc = await AccountStore.findOne({ AccountNumber: req.body.AccountNumber })
-        console.log(Acc)
-        switch (res.locals.valid_group) {
-            case !(Acc.SupportedRegions.includes(req.body.Region)):
-                res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: `${req.body.Region} is not supported in ${req.body.AccountNumber}` })
-                break;
-            case !(Acc.IAMRole && Acc.KMSKey):
-                res.status(StatusCodes.FAILED_DEPENDENCY).json({ error: `${req.body.AccountNumber} setup is not complete yet.` })
-                break;
-            case true:
-                res.status(StatusCodes.CREATED).json({ msg: req.body })
-                break;
-            default:
-                res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: `The user is not authorized to upload secrets to ${req.body.Cognito_group} group.` })
-                break;
+        try {
+            const Acc = await AccountStore.findOne({ AccountNumber: req.body.AccountNumber })
+            console.log(Acc)
+            switch (res.locals.valid_group) {
+                //Check cases here
+                case (!Acc.SupportedRegions.includes(req.body.Region)):
+                    res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: `${req.body.Region} is not supported in ${req.body.AccountNumber}` })
+                    break;
+                case !(Acc.IAMRole && Acc.KMSKey):
+                    res.status(StatusCodes.FAILED_DEPENDENCY).json({ error: `${req.body.AccountNumber} setup is not complete yet.` })
+                    break;
+                case true:
+                    res.status(StatusCodes.CREATED).json({ msg: req.body })
+                    break;
+                default:
+                    res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: `The user is not authorized to upload secrets to ${req.body.Cognito_group} group.` })
+                    break;
+            }
+        } catch (error) {
+            res.status(StatusCodes.BAD_REQUEST).json({ error: error.name, message: error.message })
         }
     }
     else{
